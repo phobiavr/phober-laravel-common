6b9be12df120971767fd3b0fbdcd8dfbd18db943
Hikmat Abdukhaligov <hikmat.pou@gmail.com>
2024-09-24 05:14:41 +0400

    update: laravel-common package

--- src/Clients/NotificationClient.php
+++ src/Clients/NotificationClient.php
@@ -3,16 +3,28 @@
 namespace Phobiavr\PhoberLaravelCommon\Clients;
 
 use GuzzleHttp\Promise\PromiseInterface;
+use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
 use Phobiavr\PhoberLaravelCommon\Enums\NotificationChannel;
 use Phobiavr\PhoberLaravelCommon\Enums\NotificationProvider;
 
 class NotificationClient {
-    protected static ?string $url = 'http://notification-server';
+    protected static ?string $url = null;
 
+    public static function getUrl(): string {
+        return self::$url ?? env('NOTIFICATION_SERVER_URL', 'http://notification-server');
+    }
+
+    /**
+     * @param NotificationProvider $provider
+     * @param NotificationChannel $channel
+     * @param string $message
+     * @return PromiseInterface|Response
+     * @throws ConnectionException
+     */
     public static function sendMessage(NotificationProvider $provider, NotificationChannel $channel, string $message): PromiseInterface|Response {
-        return Http::accept('application/json')->post(self::$url . '/', [
+        return Http::accept('application/json')->post(self::getUrl() . '/', [
             'provider' => $provider->value,
             'channel'  => $channel->value,
             'message'  => $message,
